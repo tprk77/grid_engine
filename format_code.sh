612d399101e539f27aeabc92cdada2ddfd363cdf
Tim Perkins <code@tprk77.net>
2019-06-30 16:56:35 -0400

    Fixed formatting script

--- format_code.sh
+++ format_code.sh
@@ -7,17 +7,18 @@ set -o nounset
 set -o pipefail
 IFS=$'\n\t'
 
-readonly C_FILENAME_REGEX=".*\.[ch]$"
-readonly C_LOCKFILE_REGEX=".*\.#.+\.[ch]$"
+readonly C_FILENAME_REGEX='.*\.[ch]$'
+readonly C_LOCKFILE_REGEX='.*\.#.+\.[ch]$'
+readonly DIRS='"src" "include" "test"'
 
 readonly SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-readonly lockfiles="$(cd "${SCRIPT_DIR}" && find "src" "include" -regex "${C_LOCKFILE_REGEX}")"
+readonly lockfiles="$(cd "${SCRIPT_DIR}" && eval "find ${DIRS} -regex '${C_LOCKFILE_REGEX}'")"
 if [ -n "${lockfiles}" ]; then
     echo "Emacs Lockfiles detected! Save your files!" 2>&1
     exit 1
 fi
 
-(cd "${SCRIPT_DIR}" && find "src" "include" -regex "${C_FILENAME_REGEX}" | xargs clang-format -i)
+(cd "${SCRIPT_DIR}" && eval "find ${DIRS} -regex '${C_FILENAME_REGEX}'" | xargs clang-format -i)
 
 exit 0
