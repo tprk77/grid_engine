c659903bc8f7f23ac186cf766ba9d724b4297b92
Tim Perkins <code@tprk77.net>
2021-05-31 16:35:36 -0400

    Slight cleanup of Langton demo

--- demos/demo_langton.c
+++ demos/demo_langton.c
@@ -3,23 +3,48 @@
 // Licensed under an MIT style license, see LICENSE.md for details.
 // You are free to copy and modify this code. Happy hacking!
 
-#include <stdio.h>
 #include "grid_engine/grid_engine.h"
 
-int turn(int, int);
+#include <stdbool.h>
+#include <stdio.h>
 
-enum direction { North, East, South, West };
+typedef enum direction {
+  DIRECTION_NORTH = 0,
+  DIRECTION_EAST = 1,
+  DIRECTION_SOUTH = 2,
+  DIRECTION_WEST = 3,
+} direction_t;
 
 typedef struct user_data {
   size_t last_update_time_s;
   ge_grid_t* temp_grid;
   ge_coord_t ant_coords;
-  enum direction orientation;
+  direction_t orientation;
 } user_data_t;
+
+int turn(int direction, int live)
+{
+  if (live) {
+    if (direction < DIRECTION_WEST) {
+      return ++direction;
+    }
+    else {
+      return DIRECTION_NORTH;
+    }
+  }
+  else {
+    if (direction > DIRECTION_NORTH) {
+      return --direction;
+    }
+    else {
+      return DIRECTION_WEST;
+    }
+  }
+}
 
 void langton_loop_func(ge_grid_t* grid, void* user_data_, uint32_t time_ms)
 {
-  int antFound = 0;
+  bool ant_found = false;
   // Cast the user data back to the right type
   uint8_t val;
   user_data_t* user_data = (user_data_t*) user_data_;
@@ -33,37 +58,28 @@ void langton_loop_func(ge_grid_t* grid, void* user_data_, uint32_t time_ms)
   const size_t width = ge_grid_get_width(grid);
   const size_t height = ge_grid_get_height(grid);
   // Compute the new grid from the old grid
-
-  // printf("(%I64ld, %I64ld)\n", user_data->ant_coords.x, user_data->ant_coords.y);
-
   for (size_t jj = 0; jj < height; ++jj) {
     for (size_t ii = 0; ii < width; ++ii) {
       const ge_coord_t coord = (ge_coord_t){ii, jj};
       const bool is_live = (ge_grid_get_coord(grid, coord) != 0);
-
       // Apply the Langton Ant rules
-      if (coord.x == user_data->ant_coords.x && coord.y == user_data->ant_coords.y && !antFound) {
-        antFound = 1;
-        // printf("Start: %d", user_data->orientation);
+      if (coord.x == user_data->ant_coords.x && coord.y == user_data->ant_coords.y && !ant_found) {
+        ant_found = true;
         user_data->orientation = turn(user_data->orientation, is_live);
-        // printf("End: %d", user_data->orientation);
-        // printf("match\n");
-
         switch (user_data->orientation) {
-        case North:
+        case DIRECTION_NORTH:
           user_data->ant_coords.y--;
           break;
-        case East:
+        case DIRECTION_EAST:
           user_data->ant_coords.x++;
           break;
-        case South:
+        case DIRECTION_SOUTH:
           user_data->ant_coords.y++;
           break;
-        case West:
+        case DIRECTION_WEST:
           user_data->ant_coords.x--;
           break;
         }
-
         if (is_live)
           ge_grid_set_coord(user_data->temp_grid, coord, 0);
         else
@@ -80,39 +96,18 @@ void langton_loop_func(ge_grid_t* grid, void* user_data_, uint32_t time_ms)
   ge_grid_clear_pixel_arr(user_data->temp_grid);
 }
 
-int turn(int direction, int live)
-{
-  if (live) {
-    if (direction < West) {
-      return ++direction;
-    }
-    else {
-      return North;
-    }
-  }
-  else {
-    if (direction > North) {
-      return --direction;
-    }
-    else {
-      return West;
-    }
-  }
-}
-
 int main(void)
 {
   const size_t width = 100;
   const size_t height = 100;
   ge_grid_t* grid = ge_grid_create(width, height);
-
   // User data to track state, etc
-
-  user_data_t user_data = {.last_update_time_s = 0,
-                           .temp_grid = ge_grid_create(width, height),
-                           .ant_coords = (ge_coord_t){49, 49},
-                           .orientation = East};
-
+  user_data_t user_data = {
+      .last_update_time_s = 0,
+      .temp_grid = ge_grid_create(width, height),
+      .ant_coords = (ge_coord_t){49, 49},
+      .orientation = DIRECTION_EAST,
+  };
   // The EZ loop data
   ez_loop_data_t ez_loop_data = {
       .grid = grid,
