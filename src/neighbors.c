914bee94f78a270a8968da817a6d9166d651e231
Tim Perkins <code@tprk77.net>
2021-06-13 23:47:10 -0400

    Improve direction code

--- src/neighbors.c
+++ src/neighbors.c
@@ -11,8 +11,14 @@
 
 static bool coord_inside(ge_coord_t coord, size_t width, size_t height);
 
-static const ge_coord_t GE_NEIGHBOR_OFFSETS[GE_MAX_NUM_NEIGHBORS] = {
-    {0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}};
+static const ge_direction_t GE_DIRECTION_OPPOSITES[GE_NUM_DIRECTIONS] = {
+    GE_DIRECTION_SOUTH, GE_DIRECTION_SOUTHWEST, GE_DIRECTION_WEST, GE_DIRECTION_NORTHWEST,
+    GE_DIRECTION_NORTH, GE_DIRECTION_NORTHEAST, GE_DIRECTION_EAST, GE_DIRECTION_SOUTHEAST,
+};
+
+static const ge_coord_t GE_DIRECTION_OFFSETS[GE_NUM_DIRECTIONS] = {
+    {0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1},
+};
 
 static const ge_neighbors_t GE_NEIGHBORS_DEFAULTS = {
     .neighbors =
@@ -28,11 +34,29 @@ static const ge_neighbors_t GE_NEIGHBORS_DEFAULTS = {
         },
 };
 
+ge_direction_t ge_direction_get_opposite(ge_direction_t direction)
+{
+  if (direction == GE_DIRECTION_NONE || direction == GE_DIRECTION_ALL) {
+    GE_LOG_ERROR("Cannot get opposite for special direction ALL or NONE!");
+    abort();
+  }
+  return GE_DIRECTION_OPPOSITES[direction];
+}
+
+ge_coord_t ge_direction_get_offset(ge_direction_t direction)
+{
+  if (direction == GE_DIRECTION_NONE || direction == GE_DIRECTION_ALL) {
+    GE_LOG_ERROR("Cannot get offset for special direction ALL or NONE!");
+    abort();
+  }
+  return GE_DIRECTION_OFFSETS[direction];
+}
+
 ge_neighbors_t ge_neighbors_from_coord(ge_coord_t coord)
 {
   ge_neighbors_t neighbors = GE_NEIGHBORS_DEFAULTS;
-  for (size_t ii = 0; ii < GE_MAX_NUM_NEIGHBORS; ++ii) {
-    neighbors.neighbors[ii] = ge_coord_add(coord, GE_NEIGHBOR_OFFSETS[ii]);
+  for (size_t ii = 0; ii < GE_NUM_DIRECTIONS; ++ii) {
+    neighbors.neighbors[ii] = ge_coord_add(coord, GE_DIRECTION_OFFSETS[ii]);
   }
   return neighbors;
 }
@@ -40,8 +64,8 @@ ge_neighbors_t ge_neighbors_from_coord(ge_coord_t coord)
 ge_neighbors_t ge_neighbors_from_coord_inside(ge_coord_t coord, size_t width, size_t height)
 {
   ge_neighbors_t neighbors = GE_NEIGHBORS_DEFAULTS;
-  for (size_t ii = 0; ii < GE_MAX_NUM_NEIGHBORS; ++ii) {
-    ge_coord_t nbr_coord = ge_coord_add(coord, GE_NEIGHBOR_OFFSETS[ii]);
+  for (size_t ii = 0; ii < GE_NUM_DIRECTIONS; ++ii) {
+    ge_coord_t nbr_coord = ge_coord_add(coord, GE_DIRECTION_OFFSETS[ii]);
     if (!coord_inside(nbr_coord, width, height)) {
       continue;
     }
@@ -53,9 +77,9 @@ ge_neighbors_t ge_neighbors_from_coord_inside(ge_coord_t coord, size_t width, si
 ge_neighbors_t ge_neighbors_from_coord_wrapped(ge_coord_t coord, size_t width, size_t height)
 {
   ge_neighbors_t neighbors = GE_NEIGHBORS_DEFAULTS;
-  for (size_t ii = 0; ii < GE_MAX_NUM_NEIGHBORS; ++ii) {
+  for (size_t ii = 0; ii < GE_NUM_DIRECTIONS; ++ii) {
     neighbors.neighbors[ii] =
-        ge_coord_wrap(ge_coord_add(coord, GE_NEIGHBOR_OFFSETS[ii]), width, height);
+        ge_coord_wrap(ge_coord_add(coord, GE_DIRECTION_OFFSETS[ii]), width, height);
   }
   return neighbors;
 }
@@ -64,7 +88,7 @@ bool ge_neighbors_has_neighbor(const ge_neighbors_t* neighbors, ge_direction_t d
 {
   if (direction == GE_DIRECTION_NONE || direction == GE_DIRECTION_ALL) {
     const ge_coord_t* const begin_coord = neighbors->neighbors;
-    const ge_coord_t* const end_coord = neighbors->neighbors + GE_MAX_NUM_NEIGHBORS;
+    const ge_coord_t* const end_coord = neighbors->neighbors + GE_NUM_DIRECTIONS;
     for (const ge_coord_t* nbr_coord = begin_coord; nbr_coord < end_coord; ++nbr_coord) {
       const bool has_neighbor = !ge_coord_is_invalid(*nbr_coord);
       if ((direction == GE_DIRECTION_NONE && has_neighbor)
@@ -94,21 +118,12 @@ const ge_coord_t* ge_neighbors_next_coord(const ge_neighbors_t* neighbors,
                                           const ge_coord_t* prev_coord)
 {
   const ge_coord_t* const begin_coord = neighbors->neighbors;
-  const ge_coord_t* const end_coord = neighbors->neighbors + GE_MAX_NUM_NEIGHBORS;
+  const ge_coord_t* const end_coord = neighbors->neighbors + GE_NUM_DIRECTIONS;
   const ge_coord_t* coord = (prev_coord == NULL ? begin_coord : prev_coord + 1);
   while (coord < end_coord && ge_coord_is_invalid(*coord)) {
     ++prev_coord;
   }
   return (coord != end_coord ? coord : NULL);
-}
-
-ge_coord_t ge_neighbors_get_offset(ge_direction_t direction)
-{
-  if (direction == GE_DIRECTION_NONE || direction == GE_DIRECTION_ALL) {
-    GE_LOG_ERROR("Cannot get offset for special direction ALL or NONE!");
-    abort();
-  }
-  return GE_NEIGHBOR_OFFSETS[direction];
 }
 
 static bool coord_inside(ge_coord_t coord, size_t width, size_t height)
