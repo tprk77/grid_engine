21d256acd9482baf15f5cec892d8477ba6800d7f
Tim Perkins <code@tprk77.net>
2021-10-09 21:53:44 -0400

    Update format script

--- format_code.sh
+++ format_code.sh
@@ -13,12 +13,36 @@ readonly DIRS='"src" "include" "demos"'
 
 readonly SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
+function detect_clang_format {
+    readonly CLANG_FORMAT_COMMANDS=(
+        "clang-format-13"
+        "clang-format-12"
+        "clang-format-11"
+        "clang-format-10"
+        "clang-format"
+    )
+    for clang_format_cmd in "${CLANG_FORMAT_COMMANDS[@]}"; do
+        if command -v "${clang_format_cmd}" &>/dev/null; then
+            echo "${clang_format_cmd}"
+            return
+        fi
+    done
+}
+
+readonly CLANG_FORMAT_COMMAND="$(detect_clang_format)"
+
+if [ -z "${CLANG_FORMAT_COMMAND}" ]; then
+    echo "Could not detect clang-format command!" 2>&1
+    exit 1
+fi
+
 readonly lockfiles="$(cd "${SCRIPT_DIR}" && eval "find ${DIRS} -regex '${C_LOCKFILE_REGEX}'")"
 if [ -n "${lockfiles}" ]; then
     echo "Emacs Lockfiles detected! Save your files!" 2>&1
     exit 1
 fi
 
-(cd "${SCRIPT_DIR}" && eval "find ${DIRS} -regex '${C_FILENAME_REGEX}'" | xargs clang-format -i)
+(cd "${SCRIPT_DIR}" && eval "find ${DIRS} -regex '${C_FILENAME_REGEX}'" \
+    | xargs "${CLANG_FORMAT_COMMAND}" -i)
 
 exit 0
